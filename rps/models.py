c4ba35aa7a87d674d51bb11138d2d6ef2cd23c03
Augusto Roccasalva <augusto@rioplomo.com.ar>
2015-11-01 19:58:03 -0300

    Add some fields to model

--- rps/models.py
+++ rps/models.py
@@ -13,5 +13,11 @@ class Customer(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     dni = db.Column(db.Integer, nullable=False, unique=True)
+
     name = db.Column(db.String, nullable=False)
     surname = db.Column(db.String, nullable=False)
+    email = db.Column(db.String)
+    phone = db.Column(db.String)
+    twitter = db.Column(db.String)
+
+    matricula = db.Column(db.String)
